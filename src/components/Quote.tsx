2abbe397aa90eb15f2824832063cf15525fee1ce
Mohamed2670 <mh639450@gmail.com>
2025-08-17 01:11:45 +0300

    new

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState, type TextareaHTMLAttributes } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Quote() {
   const [quote, setQuote] = useState<string>("");
